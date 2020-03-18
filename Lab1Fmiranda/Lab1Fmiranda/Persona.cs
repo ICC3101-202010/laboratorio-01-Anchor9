ec01b7a58db12599e46b60e31ee9fa5292febe42
Anchor9 <fjmiranda4@miuandes.cl>
2020-03-17 21:55:59 -0300

    sister computer

---
+++ Lab1Fmiranda/Lab1Fmiranda/Persona.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab1Fmiranda
+{
+    class Persona
+    {
+        public int Age;
+        public string Name;
+        public string Lastname;
+        
+       
+
+
+
+        public void Throw()
+        {
+            
+        
+        }
+
+    }
+}
--- Lab1Fmiranda/Lab1Fmiranda/Program.cs
+++ Lab1Fmiranda/Lab1Fmiranda/Program.cs
@@ -10,6 +10,7 @@ namespace Lab1Fmiranda
     {
         static void Main(string[] args)
         {
+
         }
     }
 }
